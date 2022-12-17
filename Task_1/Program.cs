02816e95964e250aa8121e699591076661148dac
Tolik <anatolijparygin988@gmail.com>
2022-12-17 22:39:27 +0300

    массив вещественных чисел

---
+++ Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Clear();
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double[,] CreateRandomArray(int rows, int columns)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(rand.Next(-10, 11) + rand.NextDouble(), 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = EnterNumber("Введите значение m: ");
+int n = EnterNumber("Введите значение n: ");
+double[,] matrix = CreateRandomArray(m, n);
+PrintArray(matrix);
